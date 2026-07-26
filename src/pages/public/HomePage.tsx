eeecd103e74ccf8b84bf4600bc38ef4a7192c428
guman24 <gumansing.dev@gmail.com>
2026-07-26 23:24:51 +0545

    hide playground section

--- src/pages/public/HomePage.tsx
+++ src/pages/public/HomePage.tsx
@@ -2,7 +2,6 @@ import { HeroSection } from '@/features/hero/components/HeroSection'
 import { ProjectGrid } from '@/features/projects/components/public/ProjectGrid'
 import { SkillsSection } from '@/features/skills/components/public/SkillsSection'
 import { TimelineSection } from '@/features/experience/components/public/TimelineSection'
-import { PlaygroundGrid } from '@/features/playground/components/public/PlaygroundGrid'
 import { ContactSection } from '@/features/contact/components/public/ContactSection'
 
 export function HomePage() {
@@ -12,7 +11,7 @@ export function HomePage() {
       <ProjectGrid />
       <SkillsSection />
       <TimelineSection />
-      <PlaygroundGrid />
+      {/* <PlaygroundGrid /> */}
       <ContactSection />
     </>
   )
